5a47008a29ea79c127c6acca80a734b003e846a5
PrashantBtkl <prashant44backup@gmail.com>
2024-08-07 20:21:53 +0530

    add: .nojekyll
    fix: production path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+            base: process.env.NODE_ENV === 'production' ? '/PrashantBtkl.github.io' : '',
         }
 	}
 };
