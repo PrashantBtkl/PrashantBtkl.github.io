c999e4412d5425d01134408142280ce75d1f1255
PrashantBtkl <prashant44backup@gmail.com>
2024-09-07 19:50:57 +0530

    add: blog card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script>
   const cards = [
-    { title: 'Github', img: 'https://img.icons8.com/plasticine/100/github.png', text: 'Projects', link: 'https://github.com/PrashantBtkl' },
-    { title: 'Medium', img: 'https://img.icons8.com/?size=100&id=XVNvUWCvvlD9&format=png&color=000000', text: 'Bug Bounty blog', link: 'https://prashantbhatkal2000.medium.com' },
-    { title: 'LinkedIn', img: 'https://img.icons8.com/?size=100&id=Pz213it3TQxp&format=png&color=000000', text: 'Experience', link: 'https://linkedin.com/in/prashant-bhatkal' },
+ 	{ title: 'Github', img: 'https://img.icons8.com/plasticine/100/github.png', text: 'Projects', link: 'https://github.com/PrashantBtkl' },
+	{ title: 'Medium', img: 'https://img.icons8.com/?size=100&id=XVNvUWCvvlD9&format=png&color=000000', text: 'Bug Bounty blog', link: 'https://prashantbhatkal2000.medium.com' },
+	{ title: 'LinkedIn', img: 'https://img.icons8.com/?size=100&id=Pz213it3TQxp&format=png&color=000000', text: 'Experience', link: 'https://linkedin.com/in/prashant-bhatkal' },
+	{ title: 'blog', img: 'https://prashantb.dev/favicon.ico', text: 'personal blogs', link: 'https://prashantb.dev' },
   ];
 </script>
 
-<svelte:head>
-  <link href="https://fonts.googleapis.com/css?family=Gelasio" rel="stylesheet">
-</svelte:head>
 <h1>Prashant Bhatkal</h1>
 <body>
 <div class="about">
@@ -30,28 +28,26 @@ I build software with Golang, Elixir, Python and Svelte. Use the links below to 
 </body>
 
 <style>
-:global(body) {
-    font-size: 16px;
-    line-height: 1.5;
-    color: #333;
-  }
-
+@import url('https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap');
 
 h1 {
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
+	font-family: "Hind Siliguri", sans-serif;
+	text-align: center;
+	margin-left: auto;
+	margin-right: auto;
 }
 
 .about {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    min-height: 20vh;
-    max-width: 100%;
-    padding: 20px;
-    text-align: center;
+	font-family: "Hind Siliguri", sans-serif;
+	font-weight: 600;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+	min-height: 20vh;
+	max-width: 100%;
+	padding: 20px;
+	text-align: center;
 	position: relative;
 }
 
@@ -64,14 +60,16 @@ h1 {
     padding: 20px;
   }
 
-  .card {
-    background-color: #f0f0f0;
-    border-radius: 8px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    margin: 10px;
-    padding: 20px;
-    width: 200px;
-    text-align: center;
+.card {
+	font-family: "Hind Siliguri", sans-serif;
+	font-weight: 600;
+	background-color: #f0f0f0;
+	border-radius: 8px;
+	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+	margin: 10px;
+	padding: 20px;
+	width: 200px;
+	text-align: center;
   }
 
   h2 {
